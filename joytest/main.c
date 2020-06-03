dc97bcf0714c63aa0fa1c5b345b505f92a736f82
Jeremy Rand <jeremy@rand-family.com>
2020-06-02 22:49:01 -0400

    Initial Commit

---
+++ joytest/main.c
@@ -0,0 +1,20 @@
+/*
+ * main.c
+ * joytest
+ *
+ * Created by Jeremy Rand on 2020-06-02.
+ * Copyright (c) 2020 Jeremy Rand. All rights reserved.
+ *
+ */
+
+
+#include <stdio.h>
+#include <conio.h>
+
+
+int main(void)
+{
+    printf("HELLO, WORLD!\n");
+    cgetc();
+    return 0;
+}
